9d9515418a09b095513ded57c0048bea2bf36207
Mas Azra Athirah <2023436008@student.uitm.edu.my>
2025-12-18 19:03:12 +0800

    Initial commit – ElectraBill Android project

--- app/src/main/java/com/example/electrabill/AboutActivity.java
+++ app/src/main/java/com/example/electrabill/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.example.electrabill;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView tvGithub = findViewById(R.id.tvGithub);
+
+        tvGithub.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://github.com/masazrathrh/ElectraBill.git"));
+            startActivity(intent);
+        });
     }
 }
